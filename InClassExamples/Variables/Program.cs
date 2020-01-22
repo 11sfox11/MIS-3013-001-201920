688bfb24fbd968d81174b652cac4ba0aa69a44ed
Stephanie Fox <Stephanie.L.Fox-1@ou.edu>
2020-01-22 12:54:22 -0600

    Created variables for first name, last name, favorite number. Also derived crazyNumber .

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,25 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstname
+            string firstName;
+
+            //Assign a value to firstName
+            firstName = "Stephanie";
+
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 72;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Fox";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName + " has a favorite number of "
+                + favoriteNumber + "\nwhich equates to a crazy number of " + crazyNumber);
+
+            // MAKE IT DYNAMIC
+            Console.ReadKey();
         }
     }
 }
