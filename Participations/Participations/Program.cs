1b40d2e21e6d3c7e660b24d27c510cdbd90f094a
Stephanie Fox <Stephanie.L.Fox-1@ou.edu>
2020-01-29 12:46:05 -0600

    1/29/2020, created a random heads/tails generator. Conditional Coin Toss Participation.

---
+++ Participations/Participations/Program.cs
@@ -0,0 +1,43 @@
+﻿//Stephanie Fox
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables!!
+            string toss;
+
+            // ask user to pick heads or tails
+            Console.WriteLine("Heads or tails?");
+                toss = Console.ReadLine().ToLower();
+            //random generator
+            Random rnd = new Random();
+            string[] headstails = { "heads", "tails" };
+
+            int htIndex = rnd.Next(headstails.Length);
+            // display
+
+            Console.WriteLine("I got {0}", headstails[htIndex] );
+
+            if (toss == headstails[htIndex])
+            {
+                Console.WriteLine("Your guess was right!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
