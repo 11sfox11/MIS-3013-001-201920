d693ad6bf6582220cdf4c089d93946c9cde0e2ed
Stephanie Fox <Stephanie.L.Fox-1@ou.edu>
2020-02-03 13:05:52 -0600

    worked with loops

---
+++ InClassExamples2/InClassExamples2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassExamples2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables!!
+            string toss;
+            int numberOfGuesses = 0;
+            // ask user to pick heads or tails
+            Console.WriteLine("Heads or tails?");
+            toss = Console.ReadLine().ToLower();
+            //random generator
+            Random rnd = new Random();
+            string[] headstails = { "heads", "tails" };
+
+            int htIndex = rnd.Next(headstails.Length);
+            // display
+
+            Console.WriteLine("I got {0}", headstails[htIndex]);
+
+            while (toss != headstails[htIndex])
+            {
+
+                if (toss != headstails[htIndex])
+                {
+                    Console.WriteLine("Heads or tails?");
+                    toss = Console.ReadLine().ToLower();
+                    //random generator
+                     rnd = new Random();
+                     htIndex = rnd.Next(headstails.Length);
+                    // display
+
+                    Console.WriteLine("I got {0}", headstails[htIndex]);
+                }
+                else
+                {
+                    Console.WriteLine("You Win");
+
+                }
+                numberOfGuesses = numberOfGuesses + 1;
+            }
+           
+            Console.WriteLine($"It took you {numberOfGuesses.ToString("N0")} to guess right");
+            const string developer = "Stephanie Fox";
+            Console.WriteLine(developer);
+            Console.ReadKey();
+
+        }
+    }
+    }
+
---
+++ Loop_Summing/Loop_Summing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//count to 100!
+            Console.WriteLine("What number do you want to count to? >>");
+            int threshold = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= threshold; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            double sum = 0; //accumulator variable!! 
+            string answer;
+
+            do
+            {
+                Console.WriteLine("\nPlease give us a # to add >>");
+                int number = Convert.ToInt32(Console.ReadLine());
+                //sum = sum + number;
+                sum += number;
+
+                Console.WriteLine("Do you have another # to add? (y, n) >>");
+                answer = Console.ReadLine();
+
+            } while (answer == "y");
+            Console.WriteLine($"Your summation of values is {sum.ToString("N2")}");
+            Console.ReadKey();    
+        }
+    }
+}
