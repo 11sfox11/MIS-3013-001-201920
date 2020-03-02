c05f307598153df2e2fe004f558b3cf584500700
Stephanie Fox <Stephanie.L.Fox-1@ou.edu>
2020-03-02 12:55:14 -0600

    added exam calculator to main app.

--- CollectionsParticipation/CollectionsParticipation/Program.cs
+++ CollectionsParticipation/CollectionsParticipation/Program.cs
@@ -32,6 +32,34 @@ namespace CollectionsParticipation
             {
                 Console.WriteLine($"You are enrolled in {subjects[i]} {courseNumbers[i]}.");
             }
+
+            // exam calculator
+
+            Console.WriteLine("Please enter your exam grades.");
+            string prompt = "What is your next score?";
+
+            List<int> scores = new List<int>();
+            double sum = 0;
+            string more;
+            do
+            {
+                Console.WriteLine(prompt);
+                int inputnum = Convert.ToInt32(Console.ReadLine());
+                scores.Add(inputnum);
+                Console.WriteLine("Do you have another score? Yes or no?");
+                more = Console.ReadLine();
+
+            } while (more != "no");
+
+            foreach (int item in scores)
+            {
+                sum = sum + item;
+            }
+
+            double average = sum / scores.Count();
+            Console.WriteLine($"Your final exam score is {average.ToString("N3")}");
+
+
             Console.ReadKey();
         }
     }
--- CollectionsParticipation/ConsoleApp1/Program.cs
+++ CollectionsParticipation/ConsoleApp1/Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your exam grades.");
+            string prompt = "What is your next score?";
+
+            List<int> scores = new List<int>();
+            double sum = 0;
+            string more;
+            do
+            {
+                Console.WriteLine(prompt);
+                int inputnum = Convert.ToInt32(Console.ReadLine());
+                scores.Add(inputnum);
+                Console.WriteLine("Do you have another score? Yes or no?");
+                more = Console.ReadLine();
+                
+            } while (more != "no");
+
+            foreach (int item in scores)
+            {
+                sum = sum + item;
+            }
+
+            double average = sum / scores.Count();
+            Console.WriteLine($"Your final exam score is {average.ToString("N3")}");
+            Console.ReadKey();
         }
     }
 }
