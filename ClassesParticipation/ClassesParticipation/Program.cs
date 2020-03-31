186a7bed3e3c3a84976e55ad441c60d3d244a832
Stephanie Fox <Stephanie.L.Fox-1@ou.edu>
2020-03-30 21:24:01 -0500

    participation

---
+++ ClassesParticipation/ClassesParticipation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassesParticipation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor foreground;
+            ConsoleColor background;
+
+            foreground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            background = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+            TextBox box1 = new TextBox();
+            box1.BackColor = ConsoleColor.DarkGreen;
+            box1.ForeColor = ConsoleColor.DarkYellow;
+            box1.Padding = 32;
+            box1.Text = "This is a text box.";
+            box1.DisplayText();
+
+            TextBox box2 = new TextBox();
+            box2.BackColor = ConsoleColor.Cyan;
+            box2.ForeColor = ConsoleColor.DarkRed;
+            box2.Padding = 18;
+            box2.Text = "This is another text box.";
+            box2.DisplayText();
+
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassesParticipation/ClassesParticipation/TextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesParticipation
+{
+    class TextBox
+    {
+       public int Padding { get; set; }
+
+       public ConsoleColor BackColor { get; set; }
+
+       public ConsoleColor ForeColor { get; set; }
+
+       public string Text { get; set; }
+
+       public TextBox()
+        {
+            Padding = 0;
+            BackColor = default;
+            ForeColor = default;
+            Text = "";
+        }
+
+       public void DisplayText()
+        {
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Console.WriteLine(Text.PadLeft(Padding).PadRight(Padding));
+        }
+    }
+}
